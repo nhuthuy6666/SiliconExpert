0ac2b14a1d7dc71a73f9a8658002aa3fe2619824
TNlap <phnt0604@gmail.com>
2025-12-24 06:19:13 +0700

    option page footer

--- inc/option-page/option-page.php
+++ inc/option-page/option-page.php
@@ -24,6 +24,13 @@ if (function_exists('acf_add_options_page')) {
 		'menu_slug' 	=> 'theme-header-settings',
 		'parent_slug'	=> 'theme-settings',
 	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Footer Settings',
+		'menu_title'	=> 'Footer',
+		'menu_slug' 	=> 'theme-footer-settings',
+		'parent_slug'	=> 'theme-settings',
+	));
 }
 
 /**
@@ -278,7 +285,7 @@ if (function_exists('acf_add_local_field_group')) {
 				'label' => 'Resources - Button Link',
 				'name' => 'resources_modern_button_link',
 				'type' => 'url',
-				'default_value' => '#',
+				'default_value' => 'http://siliconexpert.local/',
 			),
 			array(
 				'key' => 'field_resources_modern_heading1_text',
@@ -360,7 +367,7 @@ if (function_exists('acf_add_local_field_group')) {
 				'label' => 'Company - Button Link',
 				'name' => 'company_classic_button_link',
 				'type' => 'url',
-				'default_value' => '#',
+				'default_value' => 'http://siliconexpert.local/',
 			),
 			array(
 				'key' => 'field_company_classic_heading2',
@@ -415,4 +422,467 @@ if (function_exists('acf_add_local_field_group')) {
 			),
 		),
 	));
+}
+
+if (function_exists('acf_add_local_field_group')) {
+	acf_add_local_field_group(array(
+		'key' => 'group_theme_footer',
+		'title' => 'Footer Settings',
+		'fields' => array(
+			array(
+				'key' => 'field_footer_logo',
+				'label' => 'Footer Logo',
+				'name' => 'footer_logo',
+				'type' => 'image',
+				'return_format' => 'url',
+			),
+			array(
+				'key' => 'field_footer_get_started_text',
+				'label' => 'Get Started Button Text',
+				'name' => 'footer_get_started_text',
+				'type' => 'text',
+				'default_value' => 'Get Started',
+			),
+			array(
+				'key' => 'field_footer_get_started_link',
+				'label' => 'Get Started Button Link',
+				'name' => 'footer_get_started_link',
+				'type' => 'url',
+				'default_value' => 'http://siliconexpert.local/',
+			),
+			array(
+				'key' => 'field_footer_columns_tab',
+				'label' => 'Footer Columns',
+				'name' => 'footer_columns_tab',
+				'type' => 'tab',
+				'placement' => 'top',
+			),
+			array(
+				'key' => 'field_footer_grids',
+				'label' => 'Grids',
+				'name' => 'footer_grids',
+				'type' => 'group',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_footer_grid1',
+						'label' => 'Grid 1',
+						'name' => 'grid1',
+						'type' => 'group',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_footer_grid1_show_button',
+								'label' => 'Show Button In This Grid',
+								'name' => 'show_button',
+								'type' => 'true_false',
+								'ui' => 1,
+								'default_value' => 0,
+							),
+							array(
+								'key' => 'field_footer_grid1_columns',
+								'label' => 'Columns',
+								'name' => 'columns',
+								'type' => 'repeater',
+								'layout' => 'row',
+								'button_label' => 'Add Column',
+								'max' => 2,
+								'sub_fields' => array(
+									array(
+										'key' => 'field_footer_grid1_sections',
+										'label' => 'Sections',
+										'name' => 'sections',
+										'type' => 'repeater',
+										'layout' => 'row',
+										'button_label' => 'Add Section',
+										'max' => 1,
+										'sub_fields' => array(
+											array(
+												'key' => 'field_footer_grid1_section_title',
+												'label' => 'Section Title',
+												'name' => 'title',
+												'type' => 'text',
+												'required' => 1,
+											),
+											array(
+												'key' => 'field_footer_grid1_section_links',
+												'label' => 'Links',
+												'name' => 'links',
+												'type' => 'repeater',
+												'layout' => 'row',
+												'button_label' => 'Add Link',
+												'max' => 8,
+												'sub_fields' => array(
+													array(
+														'key' => 'field_footer_grid1_link_text',
+														'label' => 'Link Text',
+														'name' => 'text',
+														'type' => 'text',
+														'required' => 1,
+													),
+													array(
+														'key' => 'field_footer_grid1_link_url',
+														'label' => 'Link URL',
+														'name' => 'url',
+														'type' => 'url',
+														'default_value' => 'http://siliconexpert.local/',
+													),
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+					array(
+						'key' => 'field_footer_grid2',
+						'label' => 'Grid 2',
+						'name' => 'grid2',
+						'type' => 'group',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_footer_grid2_show_button',
+								'label' => 'Show Button In This Grid',
+								'name' => 'show_button',
+								'type' => 'true_false',
+								'ui' => 1,
+								'default_value' => 0,
+							),
+							array(
+								'key' => 'field_footer_grid2_columns',
+								'label' => 'Columns',
+								'name' => 'columns',
+								'type' => 'repeater',
+								'layout' => 'row',
+								'button_label' => 'Add Column',
+								'max' => 2,
+								'sub_fields' => array(
+									array(
+										'key' => 'field_footer_grid2_sections',
+										'label' => 'Sections',
+										'name' => 'sections',
+										'type' => 'repeater',
+										'layout' => 'row',
+										'button_label' => 'Add Section',
+										'max' => 1,
+										'sub_fields' => array(
+											array(
+												'key' => 'field_footer_grid2_section_title',
+												'label' => 'Section Title',
+												'name' => 'title',
+												'type' => 'text',
+												'required' => 1,
+											),
+											array(
+												'key' => 'field_footer_grid2_section_links',
+												'label' => 'Links',
+												'name' => 'links',
+												'type' => 'repeater',
+												'layout' => 'row',
+												'button_label' => 'Add Link',
+												'max' => 8,
+												'sub_fields' => array(
+													array(
+														'key' => 'field_footer_grid2_link_text',
+														'label' => 'Link Text',
+														'name' => 'text',
+														'type' => 'text',
+														'required' => 1,
+													),
+													array(
+														'key' => 'field_footer_grid2_link_url',
+														'label' => 'Link URL',
+														'name' => 'url',
+														'type' => 'url',
+														'default_value' => 'http://siliconexpert.local/',
+													),
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+					array(
+						'key' => 'field_footer_grid3',
+						'label' => 'Grid 3',
+						'name' => 'grid3',
+						'type' => 'group',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_footer_grid3_show_button',
+								'label' => 'Show Button In This Grid',
+								'name' => 'show_button',
+								'type' => 'true_false',
+								'ui' => 1,
+								'default_value' => 0,
+							),
+							array(
+								'key' => 'field_footer_grid3_columns',
+								'label' => 'Columns',
+								'name' => 'columns',
+								'type' => 'repeater',
+								'layout' => 'row',
+								'button_label' => 'Add Column',
+								'max' => 2,
+								'sub_fields' => array(
+									array(
+										'key' => 'field_footer_grid3_sections',
+										'label' => 'Sections',
+										'name' => 'sections',
+										'type' => 'repeater',
+										'layout' => 'row',
+										'button_label' => 'Add Section',
+										'max' => 1,
+										'sub_fields' => array(
+											array(
+												'key' => 'field_footer_grid3_section_title',
+												'label' => 'Section Title',
+												'name' => 'title',
+												'type' => 'text',
+												'required' => 1,
+											),
+											array(
+												'key' => 'field_footer_grid3_section_links',
+												'label' => 'Links',
+												'name' => 'links',
+												'type' => 'repeater',
+												'layout' => 'row',
+												'button_label' => 'Add Link',
+												'max' => 8,
+												'sub_fields' => array(
+													array(
+														'key' => 'field_footer_grid3_link_text',
+														'label' => 'Link Text',
+														'name' => 'text',
+														'type' => 'text',
+														'required' => 1,
+													),
+													array(
+														'key' => 'field_footer_grid3_link_url',
+														'label' => 'Link URL',
+														'name' => 'url',
+														'type' => 'url',
+														'default_value' => 'http://siliconexpert.local/',
+													),
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+					array(
+						'key' => 'field_footer_grid4',
+						'label' => 'Grid 4',
+						'name' => 'grid4',
+						'type' => 'group',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_footer_grid4_show_button',
+								'label' => 'Show Button In This Grid',
+								'name' => 'show_button',
+								'type' => 'true_false',
+								'ui' => 1,
+								'default_value' => 0,
+							),
+							array(
+								'key' => 'field_footer_grid4_columns',
+								'label' => 'Columns',
+								'name' => 'columns',
+								'type' => 'repeater',
+								'layout' => 'row',
+								'button_label' => 'Add Column',
+								'max' => 2,
+								'sub_fields' => array(
+									array(
+										'key' => 'field_footer_grid4_sections',
+										'label' => 'Sections',
+										'name' => 'sections',
+										'type' => 'repeater',
+										'layout' => 'row',
+										'button_label' => 'Add Section',
+										'max' => 1,
+										'sub_fields' => array(
+											array(
+												'key' => 'field_footer_grid4_section_title',
+												'label' => 'Section Title',
+												'name' => 'title',
+												'type' => 'text',
+												'required' => 1,
+											),
+											array(
+												'key' => 'field_footer_grid4_section_links',
+												'label' => 'Links',
+												'name' => 'links',
+												'type' => 'repeater',
+												'layout' => 'row',
+												'button_label' => 'Add Link',
+												'max' => 8,
+												'sub_fields' => array(
+													array(
+														'key' => 'field_footer_grid4_link_text',
+														'label' => 'Link Text',
+														'name' => 'text',
+														'type' => 'text',
+														'required' => 1,
+													),
+													array(
+														'key' => 'field_footer_grid4_link_url',
+														'label' => 'Link URL',
+														'name' => 'url',
+														'type' => 'url',
+														'default_value' => 'http://siliconexpert.local/',
+													),
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+				),
+			),
+			array(
+				'key' => 'field_footer_bottom_tab',
+				'label' => 'Footer Bottom',
+				'name' => 'footer_bottom_tab',
+				'type' => 'tab',
+				'placement' => 'top',
+			),
+			array(
+				'key' => 'field_footer_copyright',
+				'label' => 'Copyright Text',
+				'name' => 'footer_copyright',
+				'type' => 'text',
+				'default_value' => '© 2025 SiliconExpert. All Rights Reserved.',
+			),
+			array(
+				'key' => 'field_footer_address',
+				'label' => 'Address',
+				'name' => 'footer_address',
+				'type' => 'text',
+				'default_value' => '9151 E Panorama Circle Centennial, CO 80112',
+			),
+			array(
+				'key' => 'field_footer_phone',
+				'label' => 'Phone',
+				'name' => 'footer_phone',
+				'type' => 'text',
+				'default_value' => '408.330.7575',
+			),
+			array(
+				'key' => 'field_footer_privacy_text',
+				'label' => 'Privacy Policy Text',
+				'name' => 'footer_privacy_text',
+				'type' => 'text',
+				'default_value' => 'Privacy Policy',
+			),
+			array(
+				'key' => 'field_footer_privacy_link',
+				'label' => 'Privacy Policy Link',
+				'name' => 'footer_privacy_link',
+				'type' => 'url',
+				'default_value' => 'http://siliconexpert.local/',
+			),
+			array(
+				'key' => 'field_footer_terms_text',
+				'label' => 'Terms of Use Text',
+				'name' => 'footer_terms_text',
+				'type' => 'text',
+				'default_value' => 'Terms of Use',
+			),
+			array(
+				'key' => 'field_footer_terms_link',
+				'label' => 'Terms of Use Link',
+				'name' => 'footer_terms_link',
+				'type' => 'url',
+				'default_value' => 'http://siliconexpert.local/',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'theme-footer-settings',
+				),
+			),
+		),
+	));
+}
+
+if (is_admin()) {
+	add_action('acf/validate_save_post', function () {
+		if (!function_exists('acf_add_validation_error')) {
+			return;
+		}
+		if (empty($_REQUEST['page']) || $_REQUEST['page'] !== 'theme-footer-settings') {
+			return;
+		}
+		if (empty($_POST['acf']) || !is_array($_POST['acf'])) {
+			return;
+		}
+
+		$acf = $_POST['acf'];
+		$grids_field_key = 'field_footer_grids';
+		$grids = $acf[$grids_field_key] ?? null;
+		if (empty($grids) || !is_array($grids)) {
+			return;
+		}
+
+		$grid_map = array(
+			'field_footer_grid1' => array(
+				'show_button' => 'field_footer_grid1_show_button',
+				'columns' => 'field_footer_grid1_columns',
+			),
+			'field_footer_grid2' => array(
+				'show_button' => 'field_footer_grid2_show_button',
+				'columns' => 'field_footer_grid2_columns',
+			),
+			'field_footer_grid3' => array(
+				'show_button' => 'field_footer_grid3_show_button',
+				'columns' => 'field_footer_grid3_columns',
+			),
+			'field_footer_grid4' => array(
+				'show_button' => 'field_footer_grid4_show_button',
+				'columns' => 'field_footer_grid4_columns',
+			),
+		);
+
+		$enabled_show_button = array();
+		foreach ($grid_map as $grid_key => $keys) {
+			$grid = $grids[$grid_key] ?? null;
+			if (empty($grid) || !is_array($grid)) {
+				continue;
+			}
+			if (!empty($grid[$keys['show_button']])) {
+				$enabled_show_button[] = $keys['show_button'];
+			}
+		}
+		if (count($enabled_show_button) > 1) {
+			foreach ($enabled_show_button as $field_key) {
+				acf_add_validation_error($field_key, 'Show Button can only be enabled in one grid.');
+			}
+		}
+
+		foreach ($grid_map as $grid_key => $keys) {
+			$grid = $grids[$grid_key] ?? null;
+			if (empty($grid) || !is_array($grid)) {
+				continue;
+			}
+
+			$show_button = !empty($grid[$keys['show_button']]);
+			$columns = $grid[$keys['columns']] ?? array();
+			if (!is_array($columns)) {
+				$columns = array();
+			}
+
+			if ($show_button && count($columns) > 1) {
+				acf_add_validation_error($keys['columns'], 'If "Show Button In This Grid" is enabled, this grid can only have 1 column.');
+			}
+		}
+	});
 }
--- template-parts/footer-content.php
+++ template-parts/footer-content.php
@@ -1,10 +1,17 @@
 <footer id="colophon" class="site-footer">
 	<div class="footer-content">
-		<img class="footer-logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.svg" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
-		<a class="get-started-button-mobile" href="#">
+		<?php $footer_logo = get_field('footer_logo', 'option'); ?>
+		<?php if ($footer_logo): ?>
+			<img class="footer-logo" src="<?php echo esc_url($footer_logo); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+		<?php else: ?>
+			<img class="footer-logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.svg" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+		<?php endif; ?>
+		<?php $get_started_text = get_field('footer_get_started_text', 'option') ?: 'Get Started'; ?>
+		<?php $get_started_link = get_field('footer_get_started_link', 'option') ?: '#'; ?>
+		<a class="get-started-button-mobile" href="<?php echo esc_url($get_started_link); ?>">
 			<div class="text-container">
-				<span class="get-started-text-old">Get Started</span>
-				<span class="get-started-text-new">Get Started</span>
+				<span class="get-started-text-old"><?php echo esc_html($get_started_text); ?></span>
+				<span class="get-started-text-new"><?php echo esc_html($get_started_text); ?></span>
 			</div>
 			<div class="icon-container">
 				<div class="arrow-container">
@@ -18,106 +25,90 @@
 			</div>
 		</a>
 		<div class="footer-columns">
-			<div class="footer-column">
-				<div class="footer-items">
-					<p class="footer-column-title">Solutions</p>
-					<div class="footer-column-items">
-						<p class="footer-link">Engineering</p>
-						<p class="footer-link">Supply Chain</p>
-						<p class="footer-link">Compliance/Risk</p>
-						<p class="footer-link">Solutions by Industry</p>
-					</div>
-				</div>
-				<div class="footer-items">
-					<p class="footer-column-title">Services</p>
-					<div class="footer-column-items">
-						<p class="footer-link">Professional</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="footer-column">
-				<div class="footer-items">
-					<p class="footer-column-title">Products</p>
-					<div class="footer-column-items">
-						<p class="footer-link">Part Search</p>
-						<p class="footer-link">BOM Manager</p>
-						<p class="footer-link">API</p>
-						<p class="footer-link">SE Connect</p>
-						<p class="footer-link">ACL/AML/ADL</p>
-						<p class="footer-link">SCRM</p>
-						<p class="footer-link">Open Market</p>
-						<p class="footer-link">Compliance Management</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="footer-column">
-				<div class="footer-items">
-					<p class="footer-column-title">Partner Integrations</p>
-					<div class="footer-column-items">
-						<p class="footer-link">EDA</p>
-						<p class="footer-link">PLM</p>
-						<p class="footer-link">Consulting</p>
-						<p class="footer-link">Compliance</p>
-						<p class="footer-link">Community</p>
-						<p class="footer-link">Request a Quote</p>
-						<p class="footer-link">All Partners</p>
-						<p class="footer-link">Become a Partner</p>
-					</div>
-				</div>
-				<div class="footer-items">
-					<p class="footer-column-title">Resources</p>
-					<div class="footer-column-items">
-						<p class="footer-link">Blogs</p>
-						<p class="footer-link">Whitepapers, Guides &amp; eBooks</p>
-						<p class="footer-link">On-Demand Webinars</p>
-						<p class="footer-link">Events</p>
-						<p class="footer-link">Press Releases</p>
-						<p class="footer-link">FAQs</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="footer-column footer-column--cta">
-				<div class="footer-items">
-					<p class="footer-column-title">Company</p>
-					<div class="footer-column-items">
-						<p class="footer-link">About SE</p>
-						<p class="footer-link">Careers</p>
-						<p class="footer-link">Contact Us</p>
-					</div>
-				</div>
-				<a class="get-started-button" href="#">
-					<div class="text-container">
-						<span class="get-started-text-old">Get Started</span>
-						<span class="get-started-text-new">Get Started</span>
-					</div>
-					<div class="icon-container">
-						<div class="arrow-container">
-							<div class="arrow-old">
-								<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/vector.svg" alt="">
+			<?php
+			$render_footer_grid = function ($grid) use ($get_started_link, $get_started_text) {
+				if (empty($grid) || !is_array($grid)) {
+					return;
+				}
+				$columns = $grid['columns'] ?? array();
+				if (!is_array($columns)) {
+					$columns = array();
+				}
+				$show_button = !empty($grid['show_button']);
+				if (empty($columns) && !$show_button) {
+					return;
+				}
+				?>
+				<div class="footer-column<?php echo $show_button ? ' footer-column--cta' : ''; ?>">
+					<?php foreach ($columns as $col): ?>
+						<?php
+						if (empty($col) || !is_array($col)) {
+							continue;
+						}
+						$sections = $col['sections'] ?? array();
+						if (empty($sections) || !is_array($sections)) {
+							continue;
+						}
+						foreach ($sections as $section):
+							?>
+							<div class="footer-items">
+								<p class="footer-column-title"><?php echo esc_html($section['title'] ?? ''); ?></p>
+								<div class="footer-column-items">
+									<?php if (!empty($section['links']) && is_array($section['links'])): ?>
+										<?php foreach ($section['links'] as $link): ?>
+											<p class="footer-link"><a href="<?php echo esc_url($link['url'] ?? '#'); ?>"><?php echo esc_html($link['text'] ?? ''); ?></a></p>
+										<?php endforeach; ?>
+									<?php endif; ?>
+								</div>
 							</div>
-							<div class="arrow-new">
-								<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/vector.svg" alt="">
+						<?php endforeach; ?>
+					<?php endforeach; ?>
+
+					<?php if ($show_button): ?>
+						<a class="get-started-button" href="<?php echo esc_url($get_started_link); ?>">
+							<div class="text-container">
+								<span class="get-started-text-old"><?php echo esc_html($get_started_text); ?></span>
+								<span class="get-started-text-new"><?php echo esc_html($get_started_text); ?></span>
 							</div>
-						</div>
-					</div>
-				</a>
-			</div>
+							<div class="icon-container">
+								<div class="arrow-container">
+									<div class="arrow-old">
+										<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/vector.svg" alt="">
+									</div>
+									<div class="arrow-new">
+										<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/vector.svg" alt="">
+									</div>
+								</div>
+							</div>
+						</a>
+					<?php endif; ?>
+				</div>
+				<?php
+			};
+
+			$grids = get_field('footer_grids', 'option');
+			if (!empty($grids) && is_array($grids)) {
+				foreach (array('grid1', 'grid2', 'grid3', 'grid4') as $grid_key) {
+					if (empty($grids[$grid_key]) || !is_array($grids[$grid_key])) {
+						continue;
+					}
+					$render_footer_grid($grids[$grid_key]);
+				}
+			}
+			?>
 		</div>
 	</div>
 
 	<div class="footer-bottom">
 		<div class="footer-bottom-left">
-			<p class="footer-bottom-text">© 2025 SiliconExpert. All Rights Reserved.</p>
-			<p class="footer-bottom-text">9151 E Panorama Circle Centennial, CO 80112</p>
-			<p class="footer-bottom-text">408.330.7575</p>
+			<p class="footer-bottom-text"><?php echo esc_html(get_field('footer_copyright', 'option') ?: '© 2025 SiliconExpert. All Rights Reserved.'); ?></p>
+			<p class="footer-bottom-text"><?php echo esc_html(get_field('footer_address', 'option') ?: '9151 E Panorama Circle Centennial, CO 80112'); ?></p>
+			<p class="footer-bottom-text"><?php echo esc_html(get_field('footer_phone', 'option') ?: '408.330.7575'); ?></p>
 		</div>
 
 		<div class="footer-bottom-right">
-			<p class="footer-bottom-link">Privacy Policy</p>
-			<p class="footer-bottom-link">Terms of Use</p>
+			<a href="<?php echo esc_url(get_field('footer_privacy_link', 'option') ?: '#'); ?>" class="footer-bottom-link"><?php echo esc_html(get_field('footer_privacy_text', 'option') ?: 'Privacy Policy'); ?></a>
+			<a href="<?php echo esc_url(get_field('footer_terms_link', 'option') ?: '#'); ?>" class="footer-bottom-link"><?php echo esc_html(get_field('footer_terms_text', 'option') ?: 'Terms of Use'); ?></a>
 		</div>
 	</div>
 </footer>
